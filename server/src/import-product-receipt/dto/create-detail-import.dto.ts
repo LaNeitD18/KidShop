b74e4ff329f056deebe2e787827e9a30353751cd
LaNeitD18 <phanhuytien14@gmail.com>
2021-12-11 10:12:13 +0700

    Finish import receipt API

--- server/src/import-product-receipt/dto/create-detail-import.dto.ts
+++ server/src/import-product-receipt/dto/create-detail-import.dto.ts
@@ -1,6 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 export class CreateDetailImportDto {
   @ApiProperty()
+  id?: number;
+
+  @ApiProperty()
   idMatHang: number;
 
   @ApiProperty()
--- server/src/import-product-receipt/import-product-receipt.controller.ts
+++ server/src/import-product-receipt/import-product-receipt.controller.ts
@@ -96,31 +96,164 @@ export class ImportProductReceiptController {
   }
 
   // TODO
-  @Get()
-  findAll() {
-    // fetch all theo idKho
-    return this.importProductReceiptService.findAll();
+  @Get('all/:warehouseId')
+  async fetchAllImportReceipts(
+    @Param('warehouseId') warehouseId: number,
+    @Res() res: Response,
+  ) {
+    try {
+      const warehouse = await this.warehouseService.findOne(
+        warehouseId.toString(),
+      );
+      if (!warehouse) {
+        return res
+          .status(HttpStatus.NOT_FOUND)
+          .json({ message: `Can not find a warehouse with id ${warehouseId}` });
+      }
+
+      const receipts = await this.importProductReceiptService.findAll(
+        warehouseId,
+      );
+      return res.status(HttpStatus.OK).json(receipts);
+    } catch (error) {
+      return res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({ message: error.message });
+    }
   }
 
   @Get(':id')
-  findOne(@Param('id') id: number) {
-    // find1 theo idKho
-    return this.importProductReceiptService.findOne(id);
+  async fetchAnImportReceipt(@Param('id') id: number, @Res() res: Response) {
+    try {
+      const receipt = await this.importProductReceiptService.findOne(id);
+      if (!receipt) {
+        return res.status(HttpStatus.NOT_FOUND).json({
+          message: `Can not find an import receipt with id ${id}`,
+        });
+      }
+      return res.status(HttpStatus.OK).json(receipt);
+    } catch (error) {
+      return res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({ message: error.message });
+    }
   }
 
   @Patch(':id')
-  update(
+  async updateImportReceipt(
     @Param('id') id: number,
-    @Body() updateImportProductReceiptDto: UpdateImportProductReceiptDto,
+    @Body() data: UpdateImportProductReceiptDto,
+    @Res() res: Response,
   ) {
-    return this.importProductReceiptService.update(
-      id,
-      updateImportProductReceiptDto,
-    );
+    if (!data) {
+      return res
+        .status(HttpStatus.BAD_REQUEST)
+        .json({ message: 'New store information is required' });
+    }
+
+    try {
+      const receipt = await this.importProductReceiptService.findOne(id);
+      if (!receipt) {
+        return res.status(HttpStatus.NOT_FOUND).json({
+          message: `Can not find an import receipt with id ${id}`,
+        });
+      }
+
+      let warehouseManager;
+      if (data.idNguoiLap.toString() != receipt.nguoiLap.id) {
+        warehouseManager = await this.userService.findOne(
+          data.idNguoiLap.toString(),
+        );
+        if (!warehouseManager) {
+          return res.status(HttpStatus.NOT_FOUND).json({
+            message: `Can not find a user with id ${data.idNguoiLap} to assign`,
+          });
+        }
+      } else {
+        warehouseManager = receipt.nguoiLap;
+      }
+
+      let warehouse;
+      if (data.idKho != receipt.kho.id) {
+        warehouse = await this.warehouseService.findOne(data.idKho.toString());
+        if (!warehouse) {
+          return res.status(HttpStatus.NOT_FOUND).json({
+            message: `Can not find a warehouse with id ${data.idKho}`,
+          });
+        }
+      } else {
+        warehouse = receipt.kho;
+      }
+
+      for (const chiTiet of data.dsChiTietPhieuNhap) {
+        const detail = await this.detailImportService.findOne(chiTiet.id);
+        if (!detail) {
+          return res.status(HttpStatus.NOT_FOUND).json({
+            message: `Can not find a detail with id ${chiTiet.id}`,
+          });
+        }
+
+        const product = await this.productService.findOne(chiTiet.idMatHang);
+        if (!product) {
+          return res.status(HttpStatus.NOT_FOUND).json({
+            message: `Can not find a product with id ${chiTiet.idMatHang}`,
+          });
+        }
+
+        detail.matHang = product;
+        detail.soLuong = chiTiet.soLuong;
+        detail.phieuNhapKho = receipt;
+        await this.detailImportService.update(detail.id, detail);
+      }
+
+      const importReceiptData: PhieuNhapKho = {
+        tongTien: data.tongTien,
+        ghiChu: data.ghiChu,
+        nguoiLap: warehouseManager,
+        kho: warehouse,
+      };
+
+      const updatedReceipt = await this.importProductReceiptService.update(
+        id,
+        importReceiptData,
+      );
+      return res.status(HttpStatus.OK).json(updatedReceipt);
+    } catch (error) {
+      return res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({ message: error.message });
+    }
   }
 
   @Delete(':id')
-  remove(@Param('id') id: number) {
-    return this.importProductReceiptService.remove(id);
+  async deleteImportReceipt(@Param('id') id: number, @Res() res: Response) {
+    try {
+      const receipt = await this.importProductReceiptService.findOne(id);
+      if (!receipt) {
+        return res
+          .status(HttpStatus.NOT_FOUND)
+          .send(`Can not find a store with id ${id} to delete`);
+      }
+
+      for (const chiTiet of receipt.dsCTPhieuNhap) {
+        const detail = await this.detailImportService.findOne(chiTiet.id);
+        if (!detail) {
+          return res.status(HttpStatus.NOT_FOUND).json({
+            message: `Can not find a detail with id ${chiTiet.id}`,
+          });
+        }
+        await this.detailImportService.remove(detail.id);
+      }
+
+      await this.importProductReceiptService
+        .remove(id)
+        .then(() =>
+          res.status(HttpStatus.OK).send('Delete import receipt successfully'),
+        );
+    } catch (error) {
+      return res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({ message: error.message });
+    }
   }
 }
